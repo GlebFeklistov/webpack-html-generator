9d5d1d74c0a052d5a3a360d3429c3d57f61ad60f
GlebFeklistov <feklistovgleb@gmail.com>
2019-02-06 19:30:22 +0300

    chore(release): fix tokens

--- test/window.test.js
+++ test/window.test.js
@@ -7,6 +7,13 @@ describe('window params', () => {
     expect(result).to.be.a('null');
   });
 
+  it('empty object', () => {
+    const result = window({});
+    expect(result).to.be.a('string')
+      .with.have.string('<script type="text/javascript">')
+      .with.have.string('</script>')
+  });
+
   it('object', () => {
     const result = window({
       __INIT__: JSON.stringify({name: 'John Doe'})
